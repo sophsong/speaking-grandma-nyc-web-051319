39989eadc4392878a5c624706909ab114adea4d6
Sophia Song <ososanne@gmail.com>
2019-05-06 19:44:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma (phrase)
   if phrase== "I LOVE YOU GRANDMA!"
     return  "I LOVE YOU TOO PUMPKIN!"
 end
-    if phrase=="WHAT ?".upcase
+    if phrase==phrase.upcase
     return "NO, NOT SINCE 1938!"
   else 
     return "HUH?! SPEAK UP, SONNY!"
