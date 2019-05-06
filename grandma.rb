4364a78adb9803b64577090ebb4ba3ac452c8087
Sophia Song <ososanne@gmail.com>
2019-05-06 19:42:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,6 @@
+require 'pry'
 def speak_to_grandma (phrase)
+  binding.pry
   if phrase== "I LOVE YOU GRANDMA!"
     return  "I LOVE YOU TOO PUMPKIN!"
 end
@@ -6,6 +8,7 @@ end
     return "NO, NOT SINCE 1938!"
   else 
     return "HUH?! SPEAK UP, SONNY!"
+    
 end
   
 end
